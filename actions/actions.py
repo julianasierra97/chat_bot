33992041079bf2b1e734ae20c1c0e9d2c1290859
CentofantiEze <42658822+CentofantiEze@users.noreply.github.com>
2021-04-30 17:11:47 +0200

    place form working

--- actions/actions.py
+++ actions/actions.py
@@ -11,10 +11,11 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
 
 import requests
 import json
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from urllib.parse import urlencode
 
 import time
@@ -97,3 +98,91 @@ class ActionHelloWorld(Action):
         time.sleep(3.0)
 
         return []
+
+
+
+class ValidateWorkForm(FormValidationAction):
+    """Example of a form validation action."""
+
+    def name(self) -> Text:
+        return "validate_work_form"
+
+    @staticmethod
+    def place_db() -> List[Text]:
+        """Database of supported cuisines."""
+        return open('cityDb.txt', 'r').read().splitlines()
+        
+
+    # @staticmethod
+    # def is_int(string: Text) -> bool:
+    #     """Check if a string is an integer."""
+
+    #     try:
+    #         int(string)
+    #         return True
+    #     except ValueError:
+    #         return False
+
+
+
+    def validate_place(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate place value."""
+
+        if value.lower() in self.place_db():
+            # validation succeeded, set the value of the "place" slot to value
+            return {"place": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_place")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"place": None}
+
+
+
+    # def validate_num_people(
+    #     self,
+    #     value: Text,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: Dict[Text, Any],
+    # ) -> Dict[Text, Any]:
+    #     """Validate num_people value."""
+
+    #     if self.is_int(value) and int(value) > 0:
+    #         return {"num_people": value}
+    #     else:
+    #         dispatcher.utter_message(response="utter_wrong_num_people")
+    #         # validation failed, set slot to None
+    #         return {"num_people": None}
+
+    # def validate_outdoor_seating(
+    #     self,
+    #     value: Text,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: Dict[Text, Any],
+    # ) -> Dict[Text, Any]:
+    #     """Validate outdoor_seating value."""
+
+    #     if isinstance(value, str):
+    #         if "out" in value:
+    #             # convert "out..." to True
+    #             return {"outdoor_seating": True}
+    #         elif "in" in value:
+    #             # convert "in..." to False
+    #             return {"outdoor_seating": False}
+    #         else:
+    #             dispatcher.utter_message(response="utter_wrong_outdoor_seating")
+    #             # validation failed, set slot to None
+    #             return {"outdoor_seating": None}
+
+    #     else:
+    #         # affirm/deny was picked up as True/False by the from_intent mapping
+    #         return {"outdoor_seating": value}
+
